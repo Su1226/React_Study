0e1c2deadb78cdbbdf85a45003ab464ddf46ecbd
Su1226 <br12232620@gmail.com>
2025-06-16 17:57:38 +0900

    feat: 20250616 / Sign up Refactoring

--- src/Auth/Signup/Signup.jsx
+++ src/Auth/Signup/Signup.jsx
@@ -9,210 +9,179 @@ import { IoEye, IoEyeOff } from 'react-icons/io5';
  * - 데이터의 값을 받기 전에 올바른 형태인지 확인하는 절차 
  */
 
-function Signup(props) {
+function useSignInAndUpInput({ type, name, placeholder, value, vaild }) {
+    const STATUS = {
+        idle: "",
+        success: "success",
+        error: "error"
+    }
+    const [ inputValue, setInputValue ] = useState(value);
+    const [ status, setStatus ] = useState(STATUS.idle);
 
-     const [ inputState, setInputState ] = useState({
-        username: {
-            value: "",
-            message: "아이디는 영문, 숫자를 포함 4~20자여야 합니다.",
-            regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
-            status: "idle", //success(성공), error(오류), idle(초기 대기상태)
-        },
-        password: {
-            value: "",
-            message: "비밀번호는 8~20자이며, 영문·숫자·특수문자를 모두 포함해야 합니다.",
-            regex: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,20}$/,
-            status: "idle",
-        },
-        checkPassword: {
-            value: "",
-            message: "비밀번호가 서로 일치하지 않습니다.",
-            status: "idle",
-        },
-        fullName: {
-            value: "",
-            message: "이름은 한글 2~20자여야 합니다.",
-            regex: /^[가-힣]{2,20}$/,
-            status: "idle",
-        },
-        email: {
-            value: "",
-            message: "유효하지 않은 이메일 형식입니다.",
-            regex: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-            status: "idle",
+    const handleOnChange = (e) => {
+        console.log(e.target.value);
+        setInputValue(e.target.value);
+    }   // 값 입력 
+
+    const handleOnBlur = (e) => {
+        if(isEmpty(e.target.value)) {
+            setStatus(STATUS.idle);
+            return;
+        } // 값이 비어있으면 아무 동작 하지 않음.
+          // INPUT에 빈 값일때마다, 값을 초기화 시켜준다.  
+
+        if (vaild.enabled) {
+            // 유효성 검사가 있는 것이라면?
+            setStatus(vaild.regex.test(e.target.value) ? STATUS.success : STATUS.error);
+            return;
         }
-    });
+    }
 
-    const [ showPassword, setShowPassword ] = useState(false);
-    // password를 보이게 할지 결정하는 상태 변수
+    const isEmpty = (str) => {
+        return !/^.+$/.test(str);
+    }
+
+    return {
+        inputValue,
+        element: <SignInAndUpInput 
+        type={type} 
+        name={name} 
+        placeholder={placeholder} 
+        value={inputValue} 
+        vaild={vaild}
+        onChange={handleOnChange}
+        onBlur={handleOnBlur}
+        status={status}
+        message={vaild.message}/>
+    }
+}
+
+function SignInAndUpInput({type, name, placeholder, value, onChange, onBlur, status, message}) {
+    // 매개변수가 {}로 감싸져 있으므로, 비구조 할당으로 가져와야 한다. 
+    // Input만 관련된 내용을 넣을 함수.
+
+    return (
+        <div css={s.inputItem}>
+            <div css={s.inputContainer(status)}>
+                <input type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} onBlur={onBlur} />
+                    {
+                        status !== "idle"
+                        && (
+                            status === "success" 
+                            ? <div><MdOutlineCheckCircle /></div>
+                            : <div><MdOutlineErrorOutline /></div>
+                        )
+                    }
+            </div>
+            <InputVaildatedMessage status={status} message={message}/>
+        </div>
+    )
+}
+
+// 일반 함수 안에서 Hooks 함수를 사용하는 경우, 이를 Custom Hooks라고 한다. 
+// 아래의 함수를 Custom Hooks 함수로 만든다. 
+function useInputVaildatedMessage({ defaultMessage }) {
+    const STATUS = {
+        idle: "idle",
+        success: "success",
+        error: "error"
+    }   // 다음 사람이 보기 편하도록, 어떤 상태값들이 있는지 알 수 있게 변수로 작성.
+    const [ status, setStatus ] = useState(STATUS.idle);
+    const [ message, setMessage ] = useState(defaultMessage || "");
+
+    return {
+        status,
+        setStatus,
+        message,
+        setMessage,
+        element: <InputVaildatedMessage status={status} message={message} />
+    }
+}
+
+// message 관련 함수 
+function InputVaildatedMessage({status, message}) {
+    const ERROR = "error";
+    // 상수로 처리.
+
+    if (status === ERROR) {
+        return <div css={s.messageContainer()}>{message}</div>
+    }
+    
+    return <></>
+}
+
+
+// Password Hidden Button 관련 함수
+function PasswordInputHiddenButton() {
+    const [ isShow, setShow ] = useState(false);
+
+    const handleOnClick = () => {
+        setShow(prev => !prev)
+    }
+    return <p onClick={handleOnClick}>{isShow ? <IoEyeOff /> : <IoEye />}</p>
+}
+
+function Signup(props) {
 
     const [ submitDisabled, setSubmitDisabled ] = useState(true);
     // 버튼 활성화 여부를 결정하는 상태 변수 
 
-    const handleOnChange = (e) => {
-        setInputState(prev => ({
-            ...prev,
-            [e.target.name]: {
-                ...prev[e.target.name],
-                value: e.target.value,
-            }
-        }));
-    }   // target의 value 값이 변경될 때마다 값을 바꾸고 
-        // 상태값을 바꿔주는 역할을 한다. 
+    const [ inputs, setInputs ] = useState([
+        {
+            type: "text",
+            name: "username",
+            placeholder: "사용자 이름",
+            value: "",
+            vaild: {
+                regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
+                message: "아이디는 영문, 숫자를 포함 4~20자여야 합니다.",
+            },
+        },
 
-    
-    // 커서가 벗어났을 때 상태값이 바뀌어야 하기 때문에,
-    // 다시 화면을 바꾸기 위해서 다음과 같은 함수를 쓴다. 
-    // 아래는 메시지가 보이는 여부만 알려주는 함수이다. -> '사용 가능한 아이디입니다.'는 별도
-    // 정규식에 맞는 키의 value를 가져와서 
-    const handleOnBlur = (e) => {
-        if (!/^.+$/.test(inputState[e.target.name].value)) {
-            setInputState(prev => ({
-                ...prev,
-                [e.target.name]: {
-                    ...prev[e.target.name],
-                    status: "idle",
-                }
-            }));
-            return;
-        }   // 글자를 지우면 아이콘도 사라질 수 있게 status를 초기화 하는 과정. 
+        {
+            type: "password",
+            name: "password",
+            placeholder: "비밀번호",
+            value: "",
+            vaild: {
+                enabled: true,
+                regex: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,20}$/,
+                message: "비밀번호는 8~20자이며, 영문·숫자·특수문자를 모두 포함해야 합니다.",
+            },
+        },
 
-        if(e.target.name === "checkPassword" ) {
-            if(inputState.password.status ==="success") {
-                setInputState(prev => ({
-                    ...prev,
-                    checkPassword: {
-                        ...prev["checkPassword"],
-                        status: prev["checkPassword"].value === prev["password"].value ? "success" : "error",
-                    }
-                }));
-            }
-            return;
-        }   // password 값과 동일한지 checkPassword로 확인하는 과정.
-            // 맞는 경우에만 checkPassword도 success로 된다. 
+        {
+            type: "password",
+            name: "checkPassword",
+            placeholder: "비밀번호",
+            value: "",
+            vaild: {
+                enabled: false,
+                regex: null,
+                message: "비밀번호가 서로 일치하지 않습니다.",
+            },
+        }
 
-        setInputState(prev => ({
-            ...prev,
-            [e.target.name]: {
-                ...prev[e.target.name],
-                status: prev[e.target.name].regex.test(prev[e.target.name].value) ? "success" : "error",
-            }
-        }));    
-            // 정규식에 맞는 경우, 상태값을 변화한 후 
-            // 해당 값으로 바꿔준다. 
-    }
+    ]); // input과 관련된 내용들을 담을 useState 변수
 
-    useEffect(() => {
-        setSubmitDisabled(!!Object.values(inputState).map(obj => obj.status).find(status => status !== "success"));
-        // 현재 inputState는 객체 안에 객체 형태이므로
-        // 객체에서 map으로 status들만 꺼내 새로운 배열로 만든다.
-        // find()로 status값에 error나 idle이 없을 경우에만, 
-        // submitButton의 disable을 풀어준다. 
-    }, [inputState]);
+    const inputItems = inputs.map(input => useSignInAndUpInput(input));
+    // 객체로 꺼낼 수 있다.
+
+    // useEffect(() => {
+    //     setSubmitDisabled(!!Object.values(inputState).map(obj => obj.status).find(status => status !== "success"));
+    //     // 현재 inputState는 객체 안에 객체 형태이므로
+    //     // 객체에서 map으로 status들만 꺼내 새로운 배열로 만든다.
+    //     // find()로 status값에 error나 idle이 없을 경우에만, 
+    //     // submitButton의 disable을 풀어준다. 
+    // }, [inputState]);
 
      return (
         <div css={s.layout}>
             <div css={s.container}>
                 <h1 css={s.title}>회원가입</h1>
-                <div css={s.inputItem}>
-                    <div css={s.inputContainer(inputState.username.status)}>
-                        <input type="text" name='username' value={inputState.username.value} placeholder='사용자 아이디' onChange={handleOnChange} onBlur={handleOnBlur} />
-                        <div>
-                            {
-                                inputState.username.status !== "idle"
-                                && (
-                                    inputState.username.status === "success" 
-                                    ? <MdOutlineCheckCircle />
-                                    : <MdOutlineErrorOutline />
-                                )
-                            }
-                        </div>
-                    </div>
-                    {
-                        inputState.username.status === "error" &&
-                        <div css={s.messageContainer()}>{inputState.username.message}</div>
-                    }
-                </div>
-
-                <div css={s.inputItem}>
-                    <div css={s.inputContainer(inputState.password.status)}>
-                        <input type={showPassword ? "test" : "password"} name='password' value={inputState.password.value} placeholder='비밀번호' onChange={handleOnChange} onBlur={handleOnBlur} />
-                        <p onClick={() => setShowPassword(prev => !prev)}>{showPassword ? <IoEyeOff /> : <IoEye />}</p>
-                            {
-                                inputState.password.status !== "idle"
-                                && (
-                                    inputState.password.status === "success" 
-                                    ? <div><MdOutlineCheckCircle /></div>
-                                    : <div><MdOutlineErrorOutline /></div>
-                                )
-                            }
-                    </div>
-                    {
-                        inputState.password.status === "error" &&
-                        <div css={s.messageContainer()}>{inputState.password.message}</div>
-                    }
-                </div>
-
-                <div css={s.inputItem}>
-                    <div css={s.inputContainer(inputState.checkPassword.status)}>
-                        <input type={showPassword ? "test" : "password"} name='checkPassword' value={inputState.checkPassword.value} placeholder='비밀번호 확인' onChange={handleOnChange} onBlur={handleOnBlur} />
-                        <div>
-                            {
-                                inputState.checkPassword.status !== "idle"
-                                && (
-                                    inputState.checkPassword.status === "success" 
-                                    ? <MdOutlineCheckCircle />
-                                    : <MdOutlineErrorOutline />
-                                )
-                            }
-                        </div>
-                    </div>
-                    {
-                        inputState.checkPassword.status === "error" &&
-                        <div css={s.messageContainer()}>{inputState.checkPassword.message}</div>
-                    }
-                </div>
-
-                <div css={s.inputItem}>
-                    <div css={s.inputContainer(inputState.fullName.status)}>
-                        <input type="text" name='fullName' value={inputState.fullName.value} placeholder='사용자 이름' onChange={handleOnChange} onBlur={handleOnBlur} />
-                        <div>
-                            {
-                                inputState.fullName.status !== "idle"
-                                && (
-                                    inputState.fullName.status === "success" 
-                                    ? <MdOutlineCheckCircle />
-                                    : <MdOutlineErrorOutline />
-                                )
-                            }
-                        </div>
-                    </div>
-                    {
-                        inputState.fullName.status === "error" &&
-                        <div css={s.messageContainer()}>{inputState.fullName.message}</div>
-                    }
-                </div>
-
-                <div css={s.inputItem}>
-                    <div css={s.inputContainer(inputState.email.status)}>
-                        <input type="text" name='email' value={inputState.email.value} placeholder='이메일' onChange={handleOnChange} onBlur={handleOnBlur} />
-                        <div>
-                            {
-                                inputState.email.status !== "idle"
-                                && (
-                                    inputState.email.status === "success" 
-                                    ? <MdOutlineCheckCircle />
-                                    : <MdOutlineErrorOutline />
-                                )
-                            }
-                        </div>
-                    </div>
-                    {
-                        inputState.email.status === "error" &&
-                        <div css={s.messageContainer()}>{inputState.email.message}</div>
-                    }
-                </div>
-
+                {
+                    inputItems.map(inputItem => inputItem.element)
+                }
             </div>
             <button css={s.submitButton} disabled={submitDisabled}>가입하기</button>
         </div>
