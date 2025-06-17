684926b2ab76b3366018862da91be579c71d65f1
Su1226 <br12232620@gmail.com>
2025-06-17 17:35:27 +0900

    feat: 20250617 / Complete Sign Up Front, Underway  Sign In Front

--- src/Auth/Signin/Signin.jsx
+++ src/Auth/Signin/Signin.jsx
@@ -1,13 +1,211 @@
 /** @jsxImportSource @emotion/react */
+import { MdOutlineCheckCircle, MdOutlineErrorOutline } from 'react-icons/md';
 import * as s from './styles';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { IoEye, IoEyeOff } from 'react-icons/io5';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+
+/**
+ * 유효성 검사(Validation Check)
+ * - 데이터의 값을 받기 전에 올바른 형태인지 확인하는 절차 
+ */
+
+function useSignInAndUpInput({ id, type, name, placeholder, value, valid }) {
+     const STATUS = {
+        idle: "idle",
+        success: "success",
+        error: "error",
+    };
+    const inputRef = useRef();
+    const [ inputValue, setInputValue ] = useState(value);
+    const [ status, setStatus ] = useState(STATUS.idle);
+
+    const handleOnChange = (e) => {
+        // console.log(e.target.value)
+        setInputValue(e.target.value);
+    }   // 값 입력 
+
+    const handleOnBlur = (e) => {
+        if(isEmpty(e.target.value)) {
+            setStatus(STATUS.idle);
+            return;
+        } // 값이 비어있으면 아무 동작 하지 않음.
+          // INPUT에 빈 값일때마다, 값을 초기화 시켜준다.  
+
+        if (valid.enabled) {
+            setStatus(valid.regex.test(e.target.value) ? STATUS.success : STATUS.error);
+            return;
+        } // 유효성 검사가 있는 것이라면? 
+          // 유효성 검사를 한다. 
+
+        // 유효성 검사가 없는 경우, 즉 password가 맞는지 확인하는 checkPassword이다.
+        // 현재 해당 코드 안에서는 가져 올 수 없으므로, 외부에서 vaild로 값을 가져온다.
+        setStatus(valid.callback() ? STATUS.success : STATUS.error);
+    }
+
+    const isEmpty = (str) => {
+        return !/^.+$/.test(str);
+    }
+
+    return {
+        name: name,
+        value: inputValue,
+        status: status,
+        ref: inputRef,
+        element: <SignInAndUpInput 
+            key={id}
+            type={type} 
+            name={name} 
+            placeholder={placeholder} 
+            value={inputValue}
+            onChange={handleOnChange}
+            onBlur={handleOnBlur}
+            status={status}
+            message={valid.message} 
+            inputRef={inputRef}/>
+    }
+}
+
+function SignInAndUpInput({type, name, placeholder, value, onChange, onBlur, status, message, inputRef}) {
+
+    const { isShow, element: PasswordInputHiddenButton } = usePasswordInputHiddenButton();
+
+     return (
+        <div css={s.inputItem}>
+            <div css={s.inputContainer(status)}>
+                <input type={type === "password" ? isShow ? "text" : "password" : type} name={name} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} ref={inputRef}/>
+                {
+                    type === "password" && PasswordInputHiddenButton
+                }
+                {
+                    status !== "idle"
+                    && (
+                        status === "success" 
+                        ? <div><MdOutlineCheckCircle /></div>
+                        : <div><MdOutlineErrorOutline /></div>
+                    )
+                }
+            </div>
+            <InputVaildatedMessage status={status} message={message} />
+        </div>
+    )
+}
+
+// message 관련 함수 
+function InputVaildatedMessage({status, message}) {
+    const ERROR = "error";
+
+    if (status === ERROR) {
+        return <div css={s.messageContainer()}>{message}</div>
+    }
+
+    return <></>
+}
+
+
+function usePasswordInputHiddenButton() {
+    const [isShow, setShow] = useState(false);
+    
+    const handleOnClick = () => {
+        setShow(prev => !prev);
+    }
+
+    return {
+        isShow,
+        element: <PasswordInputHiddenButton isShow={isShow} onClick={handleOnClick}/>
+    }
+
+}
+
+// Password Hidden Button 관련 함수
+function PasswordInputHiddenButton({isShow, onClick}) {
+    return <p onClick={onClick}>{isShow ? <IoEyeOff /> : <IoEye />}</p>
+}
 
 function Signin(props) {
-    return (
-        <div>
-            
+    const location = useLocation();
+
+    const [ submitDisabled, setSubmitDisabled ] = useState(true);
+    // 버튼 활성화 여부를 결정하는 상태 변수 
+
+    const inputs = [
+        {
+            id: 1,
+            type: "text",
+            name: "username",
+            placeholder: "사용자이름",
+            value: location.state?.username || "",
+            valid: {
+                enabled: true,
+                regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
+                message: "아이디는 영문, 숫자를 포함 4~20자여야 합니다.",
+            },
+        },
+        {
+            id: 2,
+            type: "password",
+            name: "password",
+            placeholder: "비밀번호",
+            value: location.state?.password || "",
+            valid: {
+                enabled: true,
+                regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
+                message: "비밀번호는 영문, 숫자를 포함 4~20자여야 합니다.",
+            },
+        },
+    ];
+
+    const inputItems = inputs.map(input => useSignInAndUpInput(input)); 
+
+    useEffect(() => {
+        inputItems.forEach(inputItem => {
+            inputItem.ref.current.focus();
+            inputItem.ref.current.blur();
+        })
+    }, [])  // 장착이 되고 나서 Ref를 참조하겠다는 의미. 최초 한 번만 일어남. 
+    
+    useEffect(() => {
+        setSubmitDisabled(!!inputItems.find(inputItem => inputItem.status !== "success"))
+    }, [inputItems]);
+
+    const handleRegisterOnClick = async () => {
+        // async가 붙은 경우? 비동기이자 Promise
+        // 어떨 때 쓰는가? await을 써야 하는 경우
+        
+        const url = "http://localhost:8080/api/users"
+
+        let data = {};
+
+        inputItems.forEach(inputItem => {
+            data = {
+                ...data,
+                [inputItem.name]: inputItem.value,
+            }
+        });
+
+        try {
+            await axios.post(url, data);      
+            alert("사용자 등록 완료");
+        } catch {
+            alert("사용자 등록 오류");
+        }
+    }
+
+
+     return (
+        <div css={s.layout}>
+            <div css={s.container}>
+                <h1 css={s.title}>로그인</h1>
+                {
+                    inputItems.map(inputItem => inputItem.element)
+                }
+            </div>
+            <button css={s.submitButton} disabled={submitDisabled} onClick={handleRegisterOnClick}>로그인 하기</button>
         </div>
     );
+    
 }
 
 export default Signin;
+
--- src/Auth/Signup/Signup.jsx
+++ src/Auth/Signup/Signup.jsx
@@ -1,25 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import { MdOutlineCheck, MdOutlineCheckCircle, MdOutlineError, MdOutlineErrorOutline } from 'react-icons/md';
+import { MdOutlineCheckCircle, MdOutlineErrorOutline } from 'react-icons/md';
 import * as s from './styles';
 import React, { useEffect, useState } from 'react';
 import { IoEye, IoEyeOff } from 'react-icons/io5';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * 유효성 검사(Validation Check)
  * - 데이터의 값을 받기 전에 올바른 형태인지 확인하는 절차 
  */
 
-function useSignInAndUpInput({ type, name, placeholder, value, vaild }) {
-    const STATUS = {
-        idle: "",
+function useSignInAndUpInput({ id, type, name, placeholder, value, valid }) {
+     const STATUS = {
+        idle: "idle",
         success: "success",
-        error: "error"
-    }
+        error: "error",
+    };
     const [ inputValue, setInputValue ] = useState(value);
     const [ status, setStatus ] = useState(STATUS.idle);
 
     const handleOnChange = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value)
         setInputValue(e.target.value);
     }   // 값 입력 
 
@@ -30,11 +32,15 @@ function useSignInAndUpInput({ type, name, placeholder, value, vaild }) {
         } // 값이 비어있으면 아무 동작 하지 않음.
           // INPUT에 빈 값일때마다, 값을 초기화 시켜준다.  
 
-        if (vaild.enabled) {
-            // 유효성 검사가 있는 것이라면?
-            setStatus(vaild.regex.test(e.target.value) ? STATUS.success : STATUS.error);
+        if (valid.enabled) {
+            setStatus(valid.regex.test(e.target.value) ? STATUS.success : STATUS.error);
             return;
-        }
+        } // 유효성 검사가 있는 것이라면? 
+          // 유효성 검사를 한다. 
+
+        // 유효성 검사가 없는 경우, 즉 password가 맞는지 확인하는 checkPassword이다.
+        // 현재 해당 코드 안에서는 가져 올 수 없으므로, 외부에서 vaild로 값을 가져온다.
+        setStatus(valid.callback() ? STATUS.success : STATUS.error);
     }
 
     const isEmpty = (str) => {
@@ -42,138 +48,202 @@ function useSignInAndUpInput({ type, name, placeholder, value, vaild }) {
     }
 
     return {
-        inputValue,
+        name: name,
+        value: inputValue,
+        status: status,
         element: <SignInAndUpInput 
-        type={type} 
-        name={name} 
-        placeholder={placeholder} 
-        value={inputValue} 
-        vaild={vaild}
-        onChange={handleOnChange}
-        onBlur={handleOnBlur}
-        status={status}
-        message={vaild.message}/>
+            key={id}
+            type={type} 
+            name={name} 
+            placeholder={placeholder} 
+            value={inputValue}
+            onChange={handleOnChange}
+            onBlur={handleOnBlur}
+            status={status}
+            message={valid.message} />
     }
+    // 실제 컴포넌트는 return 안에 있는 <SignInAndUpInput .. /> 이다. 
 }
 
 function SignInAndUpInput({type, name, placeholder, value, onChange, onBlur, status, message}) {
     // 매개변수가 {}로 감싸져 있으므로, 비구조 할당으로 가져와야 한다. 
     // Input만 관련된 내용을 넣을 함수.
 
-    return (
+    const { isShow, element: PasswordInputHiddenButton } = usePasswordInputHiddenButton();
+
+     return (
         <div css={s.inputItem}>
             <div css={s.inputContainer(status)}>
-                <input type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} onBlur={onBlur} />
-                    {
-                        status !== "idle"
-                        && (
-                            status === "success" 
-                            ? <div><MdOutlineCheckCircle /></div>
-                            : <div><MdOutlineErrorOutline /></div>
-                        )
-                    }
+                <input type={type === "password" ? isShow ? "text" : "password" : type} name={name} placeholder={placeholder} value={value} onChange={onChange} onBlur={onBlur} />
+                {
+                    type === "password" && PasswordInputHiddenButton
+                }
+                {
+                    status !== "idle"
+                    && (
+                        status === "success" 
+                        ? <div><MdOutlineCheckCircle /></div>
+                        : <div><MdOutlineErrorOutline /></div>
+                    )
+                }
             </div>
-            <InputVaildatedMessage status={status} message={message}/>
+            <InputVaildatedMessage status={status} message={message} />
         </div>
     )
 }
+// type이 password 일 때, password로 바뀌고 아니면 text나 자신의 type으로 바꾼다.
+// 또한 password일 때만, PasswordInputHiddenButton 함수를 호출하고, usePasswordInputHiddenButton로 기능을 구현.
+// isShow를 받아서 사용한 이유.
 
-// 일반 함수 안에서 Hooks 함수를 사용하는 경우, 이를 Custom Hooks라고 한다. 
-// 아래의 함수를 Custom Hooks 함수로 만든다. 
-function useInputVaildatedMessage({ defaultMessage }) {
-    const STATUS = {
-        idle: "idle",
-        success: "success",
-        error: "error"
-    }   // 다음 사람이 보기 편하도록, 어떤 상태값들이 있는지 알 수 있게 변수로 작성.
-    const [ status, setStatus ] = useState(STATUS.idle);
-    const [ message, setMessage ] = useState(defaultMessage || "");
-
-    return {
-        status,
-        setStatus,
-        message,
-        setMessage,
-        element: <InputVaildatedMessage status={status} message={message} />
-    }
-}
 
 // message 관련 함수 
 function InputVaildatedMessage({status, message}) {
     const ERROR = "error";
-    // 상수로 처리.
 
     if (status === ERROR) {
         return <div css={s.messageContainer()}>{message}</div>
     }
-    
+
     return <></>
 }
 
 
-// Password Hidden Button 관련 함수
-function PasswordInputHiddenButton() {
-    const [ isShow, setShow ] = useState(false);
-
+function usePasswordInputHiddenButton() {
+    const [isShow, setShow] = useState(false);
+    
     const handleOnClick = () => {
-        setShow(prev => !prev)
+        setShow(prev => !prev);
     }
-    return <p onClick={handleOnClick}>{isShow ? <IoEyeOff /> : <IoEye />}</p>
+
+    return {
+        isShow,
+        element: <PasswordInputHiddenButton isShow={isShow} onClick={handleOnClick}/>
+    }
+
+}
+
+// Password Hidden Button 관련 함수
+function PasswordInputHiddenButton({isShow, onClick}) {
+    return <p onClick={onClick}>{isShow ? <IoEyeOff /> : <IoEye />}</p>
 }
 
 function Signup(props) {
+    const navigate = useNavigate();
 
     const [ submitDisabled, setSubmitDisabled ] = useState(true);
     // 버튼 활성화 여부를 결정하는 상태 변수 
 
-    const [ inputs, setInputs ] = useState([
+    const inputs = [
         {
+            id: 1,
             type: "text",
             name: "username",
-            placeholder: "사용자 이름",
+            placeholder: "사용자이름",
             value: "",
-            vaild: {
+            valid: {
+                enabled: true,
                 regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
                 message: "아이디는 영문, 숫자를 포함 4~20자여야 합니다.",
             },
         },
-
         {
+            id: 2,
             type: "password",
             name: "password",
             placeholder: "비밀번호",
             value: "",
-            vaild: {
+            valid: {
                 enabled: true,
-                regex: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,20}$/,
-                message: "비밀번호는 8~20자이며, 영문·숫자·특수문자를 모두 포함해야 합니다.",
+                regex: /^(?=.*[a-z])(?=.*\d).{4,20}$/,
+                message: "비밀번호는 영문, 숫자를 포함 4~20자여야 합니다.",
+            },
+        },
+        {
+            id: 3,
+            type: "password",
+            name: "checkPassword",
+            placeholder: "비밀번호 확인",
+            value: "",
+            valid: {
+                enabled: false,
+                regex: null,
+                callback: () =>inputItems[1].inputValue === inputItems[2].inputValue,
+                message: "비밀번호가 서로 일치하지 않습니다.",
             },
         },
 
         {
-            type: "password",
-            name: "checkPassword",
-            placeholder: "비밀번호",
+            id: 4,
+            type: "text",
+            name: "fullName",
+            placeholder: "성명",
             value: "",
-            vaild: {
-                enabled: false,
-                regex: null,
-                message: "비밀번호가 서로 일치하지 않습니다.",
+            valid: {
+                enabled: true,
+                regex: /^[가-힣]{2,20}$/,
+                message: "이름은 2~20자의 한글만 입력할 수 있습니다.",
             },
-        }
+        },
 
-    ]); // input과 관련된 내용들을 담을 useState 변수
+        {
+            id: 5,
+            type: "email",
+            name: "email",
+            placeholder: "이메일",
+            value: "",
+            valid: {
+                enabled: true,
+                regex: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                message: "유효하지 않은 이메일 형식입니다.",
+            },
+        },
+    ];
 
-    const inputItems = inputs.map(input => useSignInAndUpInput(input));
+    const inputItems = inputs.map(input => useSignInAndUpInput(input)); 
     // 객체로 꺼낼 수 있다.
+    // [input, input, input, ... ] -> [{}] 가 된다. 
+    // 그래서 useSignInAndUpInput의 return 결과 객체이다. 
+    // [useSignInAndUpInput(리턴값), useSignInAndUpInput(리턴값) ... ] 과 같은 형태로 반환. 
 
-    // useEffect(() => {
-    //     setSubmitDisabled(!!Object.values(inputState).map(obj => obj.status).find(status => status !== "success"));
-    //     // 현재 inputState는 객체 안에 객체 형태이므로
-    //     // 객체에서 map으로 status들만 꺼내 새로운 배열로 만든다.
-    //     // find()로 status값에 error나 idle이 없을 경우에만, 
-    //     // submitButton의 disable을 풀어준다. 
-    // }, [inputState]);
+    useEffect(() => {
+        setSubmitDisabled(!!inputItems.find(inputItem => inputItem.status !== "success"))
+    }, [inputItems]);
+    // 상태가 바뀌면, 안에 있는 정보들이 바뀌며 반응을 할 것이다. 
+    // 즉, 여기서부터 다시 랜더링이 일어날 것이다. 
+    // 그러면 return 안의 {} 값이 변하게 된다. 
+
+    const handleRegisterOnClick = async () => {
+        // async가 붙은 경우? 비동기이자 Promise
+        // 어떨 때 쓰는가? await을 써야 하는 경우
+
+        const url = "http://localhost:8080/api/users"
+
+        let data = {};
+
+        inputItems.forEach(inputItem => {
+            data = {
+                ...data,
+                [inputItem.name]: inputItem.value,
+            }
+        }); // 데이터 형태는 DTO와 같아야 한다.
+
+        
+        try {
+            const response = await axios.post(url, data);  
+            alert("사용자 등록 완료");
+            navigate("/users/signin", {
+                state: {
+                    username: response.data.username, 
+                    password: inputItems.find(inputItem => inputItem.name === "password").value,
+                    // 암호화가 된 경우, 암호가 아닌 내가 쓴 비밀번호를 쓰기 위해서 위와 같이 쓴다. 
+                }});
+        } catch {
+            alert("사용자 등록 오류");
+        }
+        // await가 붙어 있기 때문에,
+        // 등록이 완료된 후에 alert를 띄운다. 
+    }
+
 
      return (
         <div css={s.layout}>
@@ -183,13 +253,15 @@ function Signup(props) {
                     inputItems.map(inputItem => inputItem.element)
                 }
             </div>
-            <button css={s.submitButton} disabled={submitDisabled}>가입하기</button>
+            <button css={s.submitButton} disabled={submitDisabled} onClick={handleRegisterOnClick}>가입하기</button>
         </div>
     );
+    
 }
 
 export default Signup;
 
+// 20250616 Sign Up 일반 
 // 위 div 구조는 
 // input을 넣고, input과 함께 있는 div은 
 // 정규식을 이용해서 가능한 것인지를 판단하는 아이콘이 들어갈 것이다.
@@ -198,9 +270,15 @@ export default Signup;
 // h1 아래의 div은 안의 내용들을 중앙으로 위치시키기 위해서 만든 div
 // 가운데 정렬을 위해 padding-bottom: 100px;를 준다.
 
+// 20250617 Clean Code
+// inpust 안에 객체를 추가해주면, 언제든지 다른 객체를 넣을 수 있다.
+// 예를 들어, 전화번호나 주소 같은 것들 전부 다 뿌려줄 수 있다. 
 
-/**
- * to. ChatGPT -> 정규식은 GPT에게 맡긴다..
- * username, password, checkPassword, fullName(한글), email
- * javascirpt 정규 표현식을 각각 만들어 주고, error메세지도 만들어줘
- */
+// 그렇다면 custom Hooks 함수는?
+// Custom Hooks 함수는 Hooks 함수인데, 함수 안에 또 다른 Hooks(use..())함수가 들어있으며,
+// 변화가 생기는 이벤트가 있는 경우에 
+// 분리를 할 수 있다. 
+
+// 굳이 이벤트가 없다면 나눌 필요가 없으며, use가 없으면 나눌 필요가 없다. 
+
+// 해당 방법은 초기에 코드를 짤 때 설계가 오래 걸리므로, 추후에 수정하는 것을 권고한다. 
